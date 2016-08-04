2270f7f8abf73e45ab9337c404912ea9133eb91e
Kristian Hole-Drabløs <kristian.hole.drablos93@gmail.com>
2016-08-04 14:21:46 +0200

    Added test for restart files

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -27,10 +27,33 @@ macro (add_test_compareECLFiles casename filename)
                TEST_ARGS ${OPM_DATA_ROOT}/${casename}/${filename}.DATA )
 endmacro (add_test_compareECLFiles)
 
+###########################################################################
+# TEST: compareECLRestartFiles 
+###########################################################################
+
+# Input: 
+#   - casename: basename (no extension) 
+#
+macro (add_test_compareECLRestartFiles casename filename)
+  
+  # Add test that runs flow and outputs the results to file
+  opm_add_test(compareECLRestartFiles_${filename} NO_COMPILE
+               EXE_NAME flow    
+	       DRIVER_ARGS ${OPM_DATA_ROOT}/${casename} ${RESULT_PATH}
+                           ${CMAKE_BINARY_DIR}/bin
+                           ${filename}
+                           ${abs_tol} ${rel_tol}
+                           ${COMPARE_SUMMARY_COMMAND} 
+                           ${COMPARE_ECL_COMMAND} 
+			   
+               TEST_ARGS ${OPM_DATA_ROOT}/${casename}/${filename})
+endmacro (add_test_compareECLRestartFiles)
+
 
 if(NOT TARGET test-suite)
   add_custom_target(test-suite)
 endif()
+
 
 opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-regressionTest.sh "")
 
@@ -38,3 +61,7 @@ add_test_compareECLFiles(spe1 SPE1CASE2)
 add_test_compareECLFiles(spe3 SPE3CASE1)
 add_test_compareECLFiles(spe9 SPE9_CP_SHORT)
 
+
+opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-restart-regressionTest.sh "")
+
+add_test_compareECLRestartFiles(spe9 SPE9_CP_SHORT)
